906688354ad06cc3d64c22710a80dfc507e97bde
mceze <marco.ceze@gmail.com>
2015-02-19 12:26:08 -0800

    renamed error macros

--- MxV_test/MxV.c
+++ MxV_test/MxV.c
@@ -20,26 +20,26 @@ int main()
   sblas_svec *V, *B=NULL;
   
   ierr = sblas_error(sblas_readsmat("A.txt", &A));
-  if (ierr != OK) return ierr;
+  if (ierr != sb_OK) return ierr;
   
   ierr = sblas_error(sblas_readsvec("R.txt", &V));
-  if (ierr != OK) return ierr;
+  if (ierr != sb_OK) return ierr;
   
   s = clock();
   //Matrix vector product
   ierr = sblas_error(sblas_smxv(1.0, A, True, V, &B));
-  if (ierr != OK) return ierr;
+  if (ierr != sb_OK) return ierr;
   e = clock();
   printf("time: %1.3e\n",(e-s)/CLOCKS_PER_SEC);
   
   //write vector out
   ierr = sblas_error(sblas_writesvecascii("b.txt", B));
-  if (ierr != OK) return ierr;
+  if (ierr != sb_OK) return ierr;
   
   
   sblas_destroysmat(A);
   sblas_destroysvec(V);
   sblas_destroysvec(B);
   
-  return OK;
+  return sb_OK;
 }
